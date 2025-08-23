87a74cc612db35c599b9ae3197902aad2d0b9223
Sathya Narayanan <Sathya.vrv@gmail.com>
2025-08-22 22:36:43 -0400

    up

--- examples/grpo_trainer/run_gemma3_270m_it_grpo.sh
+++ examples/grpo_trainer/run_gemma3_270m_it_grpo.sh
@@ -15,7 +15,7 @@ python3 -m verl.trainer.main_ppo \
     algorithm.adv_estimator=grpo \
     data.train_files=/kaggle/working/data/gsm8k/train.parquet \
     data.val_files=/kaggle/working/data/gsm8k/train.parquet \
-    data.train_batch_size=256 \
+    data.train_batch_size=64 \
     data.max_prompt_length=1024 \
     data.max_response_length=2048 \
     data.filter_overlong_prompts=True \
@@ -25,8 +25,8 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.model.enable_gradient_checkpointing=True \
     actor_rollout_ref.actor.optim.lr=2e-5 \
-    actor_rollout_ref.actor.ppo_mini_batch_size=128 \
-    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=16 \
+    actor_rollout_ref.actor.ppo_mini_batch_size=32 \
+    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=2 \
     actor_rollout_ref.actor.use_kl_loss=True \
     actor_rollout_ref.actor.kl_loss_coef=0.001 \
     actor_rollout_ref.actor.kl_loss_type=low_var_kl \
@@ -36,10 +36,10 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.actor.fsdp_config.fsdp_size=-1 \
     +actor_rollout_ref.actor.fsdp_config.wrap_policy.disable=True \
     actor_rollout_ref.rollout.name=hf \
-    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=16 \
+    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=2 \
     actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
     actor_rollout_ref.rollout.n=4 \
-    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=16 \
+    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=2 \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
     +actor_rollout_ref.ref.fsdp_config.wrap_policy.disable=True \
     algorithm.use_kl_in_reward=False \
